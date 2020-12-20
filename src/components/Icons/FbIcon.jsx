cafafece1a2f6a92f17e5a391c6d2bc2de9dd192
Tamara A <akira-49-q@yandex.ru>
2020-12-20 08:11:22 +0200

    feat: inline icons

---
+++ src/components/Icons/FbIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Icon.css'
+
+function FbIcon () {
+  return (
+
+  );
+}
+
+export default FbIcon;
---
+++ src/components/Icons/MenuIcon.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Icon.css'
+
+function MenuIcon() {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="4" y="8" width="16" height="2" fill="currentColor" />
+      <rect x="4" y="14" width="16" height="2" fill="currentColor" />
+    </svg>
+  );
+}
+
+export default MenuIcon;
